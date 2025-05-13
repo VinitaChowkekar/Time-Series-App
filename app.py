2ebdedb98ba88a6fe4d6ec627dfc12b1c0b17304
Vinita Chowkekar <148413221+VinitaChowkekar@users.noreply.github.com>
2025-05-13 07:51:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def download_chart():
 if __name__ == '__main__':
     if not os.path.exists('static'):
         os.makedirs('static')
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
